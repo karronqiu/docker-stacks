a5f45aa45d21f903dc17f80f656396f91c28745d
Karron Qiu <karron.qiu@egc.epicgames.net>
2019-06-03 20:28:07 -0700

    added guide to access windows host

--- hive-notebook/run.cmd
+++ hive-notebook/run.cmd
@@ -1,1 +1,4 @@
 docker run --rm -p 8888:8888 -v C:\\Users\karron.qiu\\Scripts\\jupyter_notebook:/home/jovyan/work --name hive-jupyter karronqiu/hive-notebook
+
+Use the following to access windows host in yaml
+docker.for.win.localhost
